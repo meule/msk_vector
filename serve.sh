f739829f9c3649c4393f4345d16850ade049f4f1
Kostia <k@varik.ru>
2014-03-26 16:44:58 +0700

    change ip

--- serve.sh
+++ serve.sh
@@ -1,6 +1,7 @@
 git pull
 cp -R nginx-moscow.conf /etc/nginx/sites-enabled/moscow
 cp -R index.html /etc/nginx/sites-enabled/moscow
+cp -R topo.html /etc/nginx/sites-enabled/moscow
 sudo service nginx restart
 kill `cat /tmp/gunicorn.pid`
 gunicorn -c gunicorn.cfg.py -p /tmp/gunicorn.pid 'TileStache:WSGITileServer("tilestache.cfg")' &
